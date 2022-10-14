7b74dacb35edf6877e07cf5d454e2499b6f1fb24
spellcry <sangiothebest@hotmail.com>
2022-10-14 15:50:22 +0200

    add comics index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,5 +3,37 @@
 @section('metaTitle', 'Comics')
 
 @section('content')
-    
+    <div class="container">
+        <ul class="comics-list">
+            @foreach ($comics as $comic)
+                <li class="list-item">
+                    <img src="{{ $comic->thumb }}" class="thumb">
+                    <p class="title">
+                        <span>Titolo: </span>
+                        {{ $comic->title }}
+                    </p>
+                    <p class="description">
+                        <span>Descrizione: </span>
+                        {{ $comic->description }}
+                    </p>
+                    <p class="price">
+                        <span>Prezzo: </span>
+                        {{ $comic->price }}
+                    </p>
+                    <p class="series">
+                        <span>Serie: </span>
+                        {{ $comic->series }}
+                    </p>
+                    <p class="sale_date">
+                        <span>Data di vendita: </span>
+                        {{ $comic->sale_date }}
+                    </p>
+                    <p class="type">
+                        <span>Tipo: </span>
+                        {{ $comic->type }}
+                    </p>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection

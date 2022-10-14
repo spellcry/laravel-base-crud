9867db335c3239ecd649705fdcd5bf941b1aa28a
spellcry <sangiothebest@hotmail.com>
2022-10-14 16:08:20 +0200

    add comics show

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('metaTitle', $comic->title)
+
+@section('content')
+    <h2 class="comic-title">{{ $comic->title }}</h2>
+    <img src="{{ $comic->thumb }}" class="thumb">
+    <ul class="comic-info__list">
+        <li class="list-item description">
+            <span>Descrizione: </span>
+            {{ $comic->description }}
+        </li>
+        <li class="list-item price">
+            <span>Prezzo: </span>
+            {{ $comic->price }}
+        </li>
+        <li class="list-item series">
+            <span>Serie: </span>
+            {{ $comic->series }}
+        </li>
+        <li class="list-item sale_date">
+            <span>Data di vendita: </span>
+            {{ $comic->sale_date }}
+        </li>
+        <li class="list-item type">
+            <span>Tipo: </span>
+            {{ $comic->type }} 
+        </li>
+    </ul>
+@endsection
